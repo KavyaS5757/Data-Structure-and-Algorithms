e7626fa0bb77ac0b3f9acdb4bd0a9e28319b4c09
Kavya Subramanian <113536933+KavyaS5757@users.noreply.github.com>
2023-07-01 22:59:27 +0530

    Add files via upload

---
+++ website-problems/2D array DS -Hackerrank.cpp
@@ -0,0 +1,52 @@
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char* ltrim(char*);
+char* rtrim(char*);
+char** split_string(char*);
+
+int parse_int(char*);
+
+/*
+ * Complete the 'hourglassSum' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+
+
+int main()      //--c code
+{
+    int i,j;
+    int arr[6][6],temp=-9999,a;
+    
+    for(i=0;i<6;i++)
+    {
+        for(j=0;j<6;j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    
+    for(i=0;i<=3;i++)
+    {
+        for(j=0;j<=3;j++)
+        {
+            a=arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2];
+            if(temp<a)
+            temp=a;
+        }
+    }
+    
+    printf("%d",temp);
+    return 0;
+}
+
