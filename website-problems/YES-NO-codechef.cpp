ac5d0a5c1e0b50e67bd07162620cbca6e6dabab5
Kavya Subramanian <113536933+KavyaS5757@users.noreply.github.com>
2023-07-02 17:51:38 +0530

    Add files via upload

---
+++ website-problems/YES-NO-codechef.cpp
@@ -0,0 +1,40 @@
+
+/*
+#include <iostream>
+using namespace std;
+
+int main() {    //water consumption  --c++17
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x;
+	    cin>>x;
+	    if(x>=2000)
+	    std::cout << "YES" << std::endl;
+	    else
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+}
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x;
+	    cin>>x;
+	    if(x>10)
+	    cout<<"NO"<<endl;
+	    else
+	    cout<<"YES"<<endl;
+	}
+	return 0;
+}
---
+++ website-problems/saving-taxes-codechef.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {    //saving taxes   --c++17 code
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,y;
+	    cin>>x>>y;
+	    printf("%d\n",x-y);
+	}
+	return 0;
+}
