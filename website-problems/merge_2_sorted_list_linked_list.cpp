299a8bc606c19b82516f5427d3d98cb592df73db
Kavya Subramanian <113536933+KavyaS5757@users.noreply.github.com>
2023-08-22 15:46:22 +0530

    Add files via upload

---
+++ website-problems/merge_2_sorted_list_linked_list.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include<iostream>
+#include<stdlib.h>
+#include <string.h>
+#include <vector>
+#include <stdlib.h>
+#include<map>
+#include <set>
+#include <unordered_map>
+#include<algorithm>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
+    struct ListNode *last = NULL, *p = list1, *q = list2, *third = NULL;
+
+    if (p == NULL)
+        return q;
+    if (q == NULL)
+        return p;
+
+    if (p->val < q->val) {
+        third = last = p;
+        p = p->next;
+        third->next = NULL;
+    } else {
+        third = last = q;
+        q = q->next;
+        third->next = NULL;
+    }
+
+    while (p && q) {
+        if (p->val < q->val) {
+            last->next = p;
+            last = p;
+            p = p->next;
+            last->next = NULL;
+        } else {
+            last->next = q;
+            last = q;
+            q = q->next;
+            last->next = NULL;
+        }
+    }
+
+    if (p)
+        last->next = p;
+    if (q)
+        last->next = q;
+
+    return third;
+}
