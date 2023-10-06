8d0f79cde7be11927599fde82dec889cd510395d
Kavya Subramanian <113536933+KavyaS5757@users.noreply.github.com>
2023-10-06 22:54:51 +0530

    Create remove-duplicates-leetcode.cpp

---
+++ website-problems/remove-duplicates-leetcode.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) 
+    {
+        int n = nums.size();
+        int j=1;
+        for(int i=1;i<nums.size();i++)
+        {
+            if(nums[i] != nums[i-1])
+            {
+                nums[j]=nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+};
+
+/*
+  https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+*/
