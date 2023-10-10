36a637d09ab07bf362711181cb5a284b9858b7c8
Kavya Subramanian <113536933+KavyaS5757@users.noreply.github.com>
2023-10-10 20:11:15 +0530

    Update Hackerrank-q1.sql

--- DBMS/Hackerrank-q1.sql
+++ DBMS/Hackerrank-q1.sql
@@ -9,3 +9,17 @@ AND COUNTRYCODE = 'USA'
 /*
  https://www.hackerrank.com/challenges/revising-the-select-query/problem?isFullScreen=true
 */
+
+ /*
+Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA.
+
+The CITY table is described as follows:
+*/
+
+SELECT NAME FROM CITY
+WHERE POPULATION > 120000
+AND COUNTRYCODE = 'USA'
+
+/*
+ https://www.hackerrank.com/challenges/revising-the-select-query-2/problem?isFullScreen=true
+*/
