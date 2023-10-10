5c714d237d90ec6874056b82b123de6911351884
Kavya Subramanian <113536933+KavyaS5757@users.noreply.github.com>
2023-10-10 19:07:19 +0530

    Create Hackerrank-q1.sql

---
+++ DBMS/Hackerrank-q1.sql
@@ -0,0 +1,11 @@
+/*Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
+
+The CITY table is described as follows:*/
+
+SELECT * FROM CITY
+WHERE POPULATION > 100000
+AND COUNTRYCODE = 'USA'
+
+/*
+ https://www.hackerrank.com/challenges/revising-the-select-query/problem?isFullScreen=true
+*/
