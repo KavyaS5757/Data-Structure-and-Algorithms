babd863ef5fc4517124cb72cecf712746c8d82d1
Kavya Subramanian <113536933+KavyaS5757@users.noreply.github.com>
2023-10-24 15:34:26 +0530

    Create to-lower-case-leetcode.cpp

---
+++ website-problems/to-lower-case-leetcode.cpp
@@ -0,0 +1,15 @@
+string toLowerCase(string s) {
+        
+        for(int i=0;s[i]!='\0';i++)
+        {
+            if(s[i]>=65 && s[i]<=90)
+            {
+                s[i]+=32;
+            }
+        }
+        return s;
+    }
+
+/*
+https://leetcode.com/problems/to-lower-case/
+*/
