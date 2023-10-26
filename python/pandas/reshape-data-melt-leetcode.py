95c49324ab5f39bdb952001809dd71cf84fcc64a
Kavya Subramanian <113536933+KavyaS5757@users.noreply.github.com>
2023-10-26 11:53:08 +0530

    Create reshape-data-melt-leetcode.py

---
+++ python/pandas/reshape-data-melt-leetcode.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def meltTable(report: pd.DataFrame) -> pd.DataFrame:
+    return report.melt(id_vars=['product'], var_name='quarter', value_name='sales')
+
+# https://leetcode.com/problems/reshape-data-melt/
