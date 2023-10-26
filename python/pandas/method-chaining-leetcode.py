431c0b425e1a0544305282b0cdf24c50ee7db176
Kavya Subramanian <113536933+KavyaS5757@users.noreply.github.com>
2023-10-26 11:37:47 +0530

    Create method-chaining-leetcode.py

---
+++ python/pandas/method-chaining-leetcode.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def findHeavyAnimals(animals: pd.DataFrame) -> pd.DataFrame:
+    return animals[animals['weight'] > 100].sort_values(['weight'], ascending = False,)[['name']]
+
+# https://leetcode.com/problems/method-chaining/
