8e8ceaf95563cf4c5b96259f2d58b85411f9ffc1
Kavya Subramanian <113536933+KavyaS5757@users.noreply.github.com>
2023-10-31 18:08:09 +0530

    Update move-all-zeroes-to-end-leetcode.cpp

--- website-problems/move-all-zeroes-to-end-leetcode.cpp
+++ website-problems/move-all-zeroes-to-end-leetcode.cpp
@@ -19,3 +19,16 @@ void pushZerosToEnd(int arr[], int n) {
 /*
 https://practice.geeksforgeeks.org/problems/move-all-zeroes-to-end-of-array0751/1
 */
+
+void pushZerosToEnd(int arr[], int n) {
+	    // code here
+	    int j=0;
+	    for(int i=0;i<n;i++)
+	   {
+	       if(arr[i]!=0)
+	       {
+	           swap(arr[i],arr[j]);
+	           j++;
+	       }
+	   }
+	}
