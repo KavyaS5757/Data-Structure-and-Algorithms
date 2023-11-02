1f574f6f92e9f0ecc58d5da50632247c4f96dd0f
Kavya Subramanian <113536933+KavyaS5757@users.noreply.github.com>
2023-11-02 20:18:18 +0530

    Create reverse-vowels-leetcode.cpp

---
+++ website-problems/reverse-vowels-leetcode.cpp
@@ -0,0 +1,30 @@
+string reverseVowels(string s) {
+        
+        string word = s;
+        int start = 0;
+        int end = s.length()-1;
+        string vowels = "aeiouAEIOU";
+
+        while(start<end)
+        {
+            while(start < end && vowels.find(word[start]) == string::npos)
+            {
+                start++;
+            }
+            while(start < end && vowels.find(word[end]) == string::npos)
+            {
+                end--;
+            }
+            swap(word[start], word[end]);
+
+            start++;
+            end--;
+        }
+
+        return word;
+
+    }
+
+/*
+https://leetcode.com/problems/reverse-vowels-of-a-string/submissions/
+*/
