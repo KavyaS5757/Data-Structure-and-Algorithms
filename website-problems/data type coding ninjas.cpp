89bfc983d85b80f8231781114c81725e52d3875b
Kavya Subramanian <113536933+KavyaS5757@users.noreply.github.com>
2024-02-11 12:17:05 +0530

    Add files via upload

---
+++ website-problems/data type coding ninjas.cpp
@@ -0,0 +1,26 @@
+int dataTypes(string type) {
+	// Write your code here
+	
+
+	if(type == "Integer")
+	{
+		return 4;
+	}
+	else if(type == "Long")
+	{
+		return 8;
+	}
+	else if(type == "Float")
+	{
+		return 4;
+	}
+	else if(type == "Double")
+	{
+		return 8;
+	}
+	else
+	{
+		return 1;
+	}
+}
+//  https://www.codingninjas.com/studio/problems/data-type_8357232?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=SUBMISSION
