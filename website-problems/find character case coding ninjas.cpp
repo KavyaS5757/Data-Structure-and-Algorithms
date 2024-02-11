96a5fcb7ce5f824e72d2cb9fc1497475014b7bd4
Kavya Subramanian <113536933+KavyaS5757@users.noreply.github.com>
2024-02-11 11:50:48 +0530

    Add files via upload

---
+++ website-problems/find character case coding ninjas.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	// Write your code here
+	char x;
+	cin>>x;
+
+	if(x>=65 && x<=90)
+	{
+	   cout<<"1";
+	}
+	else if(x>=97 && x<=122)
+	{
+	   cout<<"0";
+	}
+	else
+	{
+	   cout<<"-1";
+	}
+	
+}
+
+// https://www.codingninjas.com/studio/problems/find-character-case_58513?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM
