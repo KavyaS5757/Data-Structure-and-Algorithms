a5dac1dad839c1f594fd4d85b699b393caba7425
Kavya Subramanian <113536933+KavyaS5757@users.noreply.github.com>
2024-02-11 12:36:23 +0530

    Create if-else(decison-making) coding ninjas.cpp

---
+++ website-problems/if-else(decison-making) coding ninjas.cpp
@@ -0,0 +1,17 @@
+string compareIfElse(int a, int b) {
+	// Write your code here
+	if(a<b)
+	{
+		return "smaller";
+	}
+	else if(a>b)
+	{
+		return "greater";
+	}
+	else
+	{
+		return "equal";
+	}
+}
+
+// https://www.codingninjas.com/studio/problems/if-else-decision-making_8357235?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM
