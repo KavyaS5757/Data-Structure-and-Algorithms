584c6dd8fd9bf497f55c8b88c9e74720f3094d47
Kavya Subramanian <113536933+KavyaS5757@users.noreply.github.com>
2024-02-15 23:14:34 +0530

    Create Switch-case-statement-coding-ninjas.cpp

---
+++ website-problems/Switch-case-statement-coding-ninjas.cpp
@@ -0,0 +1,14 @@
+  double areaSwitchCase(int ch, vector<double> a) {
+	// Write your code here
+	
+	switch(ch){
+		case 1:
+
+		    return M_PI*a[0]*a[0];
+
+		case 2:
+		    return a[0]*a[1];	
+	}
+}
+
+// https://www.codingninjas.com/studio/problems/switch-case-statement_8357244?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM
