94268660e568ca33238e514feea2732785d86b53
Kavya Subramanian <113536933+KavyaS5757@users.noreply.github.com>
2024-02-16 10:29:14 +0530

    Create two-sum-leetcode-method2.cpp

---
+++ website-problems/two-sum-leetcode-method2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> _map;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            int num = nums[i];
+            int complement = target - num;
+            auto it = _map.find(complement);
+            if(it != _map.end())
+            {
+                return {it->second, i};
+            }
+            _map[num] = i;
+        }
+        return{};
+    }
+};
+
+// https://leetcode.com/problems/two-sum/description/
